cf18e7764204714f7444fcac1e26e78d240561a5
Jhorman Alexander Culma Hermosa <119307144+Alex022315@users.noreply.github.com>
2023-08-25 11:48:36 -0500

    ADD: Schemas

---
+++ flaskr/app.py
@@ -0,0 +1,20 @@
+from flaskr import create_app
+from .modelos import db, Cancion, Usuario, Album, Medio
+from .modelos import AlbumSchema
+from flask_restful import Api
+from .vistas import VistaCanciones, VistaCancion
+import enum
+
+app = create_app('default')
+app_context = app.app_context()
+app_context.push()
+
+db.init_app(app)
+db.create_all()
+
+api=Api(app)
+api.add_resource(VistaCanciones,'/caniones')
+api.add_resource(VistaCancion,'/canciones/<int:id_cancion>')
+
+
+
