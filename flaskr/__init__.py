899f0145018dd2d804dc78a16dad0934e37773e5
Jhorman Alexander Culma Hermosa <119307144+Alex022315@users.noreply.github.com>
2023-08-30 22:21:49 -0500

    ADD: Tal_Serializacion

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 #from .modelos import db, Cancion
 def create_app(config_name):
     app = Flask(__name__)
-    app.config['SQLACLCHEMY_DATABASE_URI'] ='sqlite:///tutorial_cancioes.db'
+    app.config['SQLALCHEMY_DATABASE_URL'] = 'mysql+pymysql://root@localhost/flaskmysql'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     return app
--- flaskr/app.py
+++ flaskr/app.py
@@ -1,6 +1,5 @@
 from flaskr import create_app
 from .modelos import db, Cancion, Usuario, Album, Medio
-from .modelos import AlbumSchema
 from flask_restful import Api
 from .vistas import VistaCanciones, VistaCancion
 import enum
@@ -12,7 +11,7 @@ app_context.push()
 db.init_app(app)
 db.create_all()
 
-api=Api(app)
+api= Api(app)
 api.add_resource(VistaCanciones,'/caniones')
 api.add_resource(VistaCancion,'/canciones/<int:id_cancion>')
 
